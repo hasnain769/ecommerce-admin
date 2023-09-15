db0905efc97c02de8ce8cc595777dce139367310
syed hasnain <syedhasnain769@gmail.com>
2023-09-15 16:54:15 +0500

    auth done

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function authLayout({children}:{children :React.ReactNode}){ 
+
+    return(
+        <div>
+            {children}
+        </div>
+    )
+
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+     <p>admin panel root</p>
+    )
+  }
+  
