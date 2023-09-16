8d0669330c34fe2f3750e17d4c5503a542b75fd9
syed hasnain <syedhasnain769@gmail.com>
2023-09-16 17:31:18 +0500

    progress...

--- components/models/store-model.tsx
+++ components/models/store-model.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { Model } from "../ui/model";
 import { UseStoreModel } from "@/hooks/use-store-model";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+const formSchema=z.object({
+  name :z.string().min(1),
+})
 
 export const StoreModel = () => {
   const storeModel = UseStoreModel();
-  
+  const form= useForm<z.infer<typeof formSchema>>({
+    resolver :zodResolver(formSchema)
+  })
   return(
     <Model
       title="create store"
